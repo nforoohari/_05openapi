0587a44ab13aefe1f38c9066bb4eb16f966f9ffe
Nima Foroohari <nforoohari@gmail.com>
2025-05-08 11:04:28 +0330

    Version 1

--- src/main/java/ir/digixo/controller/MyController.java
+++ src/main/java/ir/digixo/controller/MyController.java
@@ -1,29 +1,25 @@
-//package ir.digixo.controller;
-//
-//
-//import io.swagger.v3.oas.annotations.Operation;
-//import io.swagger.v3.oas.annotations.Parameter;
-//import io.swagger.v3.oas.annotations.responses.ApiResponse;
-//import io.swagger.v3.oas.annotations.responses.ApiResponses;
-//import io.swagger.v3.oas.annotations.tags.Tag;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/")
-//@Tag(name = "My Controller to define 10 services",description = "Rest Api for ...Services")
-//public class MyController {
-//
-//
-//    @GetMapping("/m")
-//    @Operation(summary = "service1 summary")
-//    @ApiResponses(value = {
-//            @ApiResponse(responseCode = "403",description = "Oauth2 contact"),
-//            @ApiResponse(responseCode = "200",description = "ok response")
-//    })
-//    public String Service1(@Parameter(name = "input1" ,description = "name of service for invoking") String str)
-//    {
-//        return "Service 1";
-//    }
-//}
+package ir.digixo.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@Tag(name = "My New Controller", description = "Adding 10 services to the RestApi")
+public class MyController {
+
+    @GetMapping("/s1")
+    @Operation(summary = "Service 1 Summary")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "403", description = "Bad Request"),
+            @ApiResponse(responseCode = "200", description = "Ok Response")})
+    public String service1(@Parameter(name = "service name", description = "Name of service to invoke") String str) {
+        return "Service 1";
+    }
+}
